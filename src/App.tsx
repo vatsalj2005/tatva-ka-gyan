cc65fc42382d647c4aa38ee00556f974328c621a
Vatsal Jain <vatsalj2005@gmail.com>
2026-03-13 08:08:25 +0000

    CSS fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-```tsx
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster as Sonner } from "@/components/ui/sonner";
@@ -45,4 +44,3 @@ export default function App() {
     </QueryClientProvider>
   );
 }
-```
